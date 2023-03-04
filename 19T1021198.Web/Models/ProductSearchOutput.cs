f5e646af717b7d4fc298eacf9b41f2e944f1928a
Vo Ngoc Nhat Quang <quangvip2611@gmail.com>
2023-03-04 22:59:13 +0700

    Add project files.

---
+++ 19T1021198.Web/Models/ProductSearchOutput.cs
@@ -0,0 +1,16 @@
+﻿using _19T1021198.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _19T1021198.Web.Models
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class ProductSearchOutput : PaginationSearchOutput
+    {
+        public List<Product> Data { get; set; }
+    }
+}
